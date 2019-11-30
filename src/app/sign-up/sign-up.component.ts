5a1d100ccf7a10de8b472081168ecb69b8fcdbcb
LeoDiduch <leo-diduch_student2019@wilder.school>
2019-11-30 16:13:28 +0100

    bug fix quest01

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SignUp } from '../sign-up';
+
 @Component({
   selector: 'sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent implements OnInit {
+
+  signUp: SignUp = {
+    email: ''
+  };
 
   constructor() { }
 
